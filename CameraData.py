06d881417c702424a5f18bc06deaf7bb3f6c7410
arjunsudheer <arjunsudheer10@gmail.com>
2021-11-11 23:22:04 -0800

    Finish VideoFileGenerator test file
    Test sample image read from video file
    
    Co-authored-by: Sumit Mantri <SumitM123@users.noreply.github.com>

--- CameraData.py
+++ CameraData.py
@@ -1,14 +1,24 @@
-class Camera Data:
+import cv2
+import math
+
+
+class CameraData:
     def __init__(self):
         self.H_FIELD_OF_VIEW = 68.37
         self.V_FIELD_OF_VIEW = 41.21
         self.CAMERA_TILT = math.radians(30)
 
-    def getHorizFOV()(self):
+    def get_horiz_FOV(self):
         return self.H_FIELD_OF_VIEW
 
-    def getVertFOV()(self):
+    def get_vert_FOV(self):
         return self.V_FIELD_OF_VIEW
 
     def get_camera_tilt(self):
         return self.CAMERA_TILT
+
+    def convert_bgr_to_rgb(self, image):
+        return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    def convert_bgr_to_yuv(self, image):
+        return cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
--- VideoFileGenerator.py
+++ VideoFileGenerator.py
@@ -1,5 +1,36 @@
-import CameraData as CD
+from CameraData import CameraData
+import cv2
+
+CD = CameraData()
 
 
 class VideoFileGenerator:
-    def __init__(self):
+    def __init__(self, file_path):
+        # set up the video capture device to get frames from the video file
+        self.input = cv2.VideoCapture(file_path)
+        # capturing variable keeps track of whether or not the video file has reached the end
+        self.capturing = True
+
+    def get_next_frame(self):
+        # read() combines the use grab() and retreive() methods in cv2 library
+        success, image = self.input.read()
+        # if there is a valid frame retrieved, then return the image and a True status, otherwise return None and a False status
+        if(success):
+            return image, True
+        else:
+            self.capturing = False
+            return None, False
+
+    def is_capturing(self):
+        return self.capturing
+
+
+# run test code
+test_video = VideoFileGenerator("/Users/arjunsudheer/Desktop/video_test_FRC.mp4")
+next_video_frame, is_valid_frame = test_video.get_next_frame()
+i = 0
+while is_valid_frame:
+    print(CD.convert_bgr_to_rgb(next_video_frame))
+    cv2.imwrite("saved_image_" + str(i) + ".jpg", next_video_frame)
+    next_video_frame, is_valid_frame = test_video.get_next_frame()
+    i += 1
